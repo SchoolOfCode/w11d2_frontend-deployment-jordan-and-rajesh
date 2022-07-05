59d0453819201d738581c251c0ccfa4997815a3b
Jordan Flash <flash.jordan.3@gmail.com>
2022-07-05 14:35:30 +0100

    adding fetch req to patch routes

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -56,7 +56,22 @@ function App() {
     setList(clearedList);
   }
 
-  function tickItem(idOfTickedItem) {
+  async function populateList() {
+    //repopulates list with all items that have a FALSE in the 'complete' column of the db
+    //look into the db for items with false in the complete column
+    //GET all items that match the criteria
+    //look at each item one by one (via id)
+  }
+
+  async function tickItem(idOfTickedItem) {
+    const response = await fetch(`${url}/items`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ id: idOfTickedItem }),
+    });
+    const data = await response.json()
+    console.log(data);
+
     setList((previous) => {
       return previous.map((item) => {
         return item.id !== idOfTickedItem
