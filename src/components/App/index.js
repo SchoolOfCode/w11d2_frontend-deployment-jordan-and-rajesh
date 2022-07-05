57d35e3a6c12b80bbcc70b0d8af7c316da88d32b
Rajesh-Reel <rajreel2@hotmail.com>
2022-07-05 11:45:32 +0100

    changed header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -66,7 +66,7 @@ function App() {
 
   return (
     <section>
-      <h1> team bruh v2</h1>
+      <h1> team reel flash v3 </h1>
       <InputList addToList={addToList} buttonText={"Add To List"} />
       <ShowList list={list} tickItem={tickItem} />
       <ClearList clearList={clearList} buttonText={"Clear List"} />
